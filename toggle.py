5922f0fd14b47c953719bd6dc8cc7c5962e6319a
BlackLutos <gcobs172250@gmail.com>
2022-09-14 20:38:38 +0800

    Add files via upload

---
+++ toggle.py
@@ -0,0 +1,17 @@
+class toggle:
+    def __init__(self) -> None:
+        pass
+    def select_interface():
+        print('select_interface')
+    def input_controll_ip():
+        print('input_controll_ip')
+    def input_ping_Target():
+        print('input_ping_Target')
+    def input_Threshold(Latency_more_than,Detenction_Period):
+        print('input_Threshold')
+    def Failback():
+        print('Failback')
+    def Ping_Target():
+        print('Ping_Target')
+if __name__=='__main__':
+    toggle.select_interface()
