e692c7e57c47b19b0ac6ca159ecde1ed57cc8abf
BlackLutos <gcobs172250@gmail.com>
2022-10-06 17:19:58 +0800

    udpate

--- wan_detect/client.py
+++ wan_detect/client.py
@@ -11,22 +11,23 @@ def start_client(addr, port):
     s = socket.socket()
     s.connect((PLC_ADDR, PLC_PORT))
     count = 0
+    pid = str(os.getpid())
     while True:
         if get_wan_state() == True:
-            msg = 'Wan_State:Normal'
+            msg = 'Wan_State:Normal' + ", pid: " + pid
         else:
             bridge_ip = get_bridge_ip()
             link_info = get_link_info()
-            msg = 'Wan_State:Abnormal, ' + 'bridge IP: ' + bridge_ip + ', link_info: ' + link_info
+            msg = 'Wan_State:Abnormal, ' + 'bridge IP: ' + bridge_ip + ', link_info: ' + link_info + ", pid: " + pid
 
         msg = msg.encode(encoding='utf-8')
         s.send(msg)
         recv_data = s.recv(1024)
         print(recv_data.decode(encoding='utf-8'))
         time.sleep(3)
-        count += 1
-        if count > 2000:
-            break
+        # count += 1
+        # if count > 2000:
+        #     break
 
     s.close()
 
--- wan_detect/wan_detect.py
+++ wan_detect/wan_detect.py
@@ -5,22 +5,35 @@ from datetime import timedelta
 import datetime
 from datetime import datetime
 from datetime import date
+import os
 
-f = open('/home/blacklutos/IGWS/wan_detect/interval.txt','r')
-interval_time = int(f.read())
-f.close()
-now_time = datetime.now() + timedelta(hours=8)
-now_time = now_time.strftime("%Y-%m-%d %H-%M-%S")
-FORMAT = '%(asctime)s %(levelname)s: %(message)s'
-logging.basicConfig(level=logging.INFO, filename = "/home/blacklutos/IGWS/wan_detect/log/wan_state_" + now_time + ".log", filemode='w')
+# f = open('/home/blacklutos/IGWS/wan_detect/interval.txt','r')
+# interval_time = int(f.read())
+# f.close()
+# now_time = datetime.now() + timedelta(hours=8)
+os.system('sudo timedatectl set-timezone Asia/Taipei')
 
-wan_state = ping('google.com')
-if wan_state:
-    logging.info("Normal")
-else:
-    logging.info("Abnormal")
+while True:
+    now_time = datetime.now()
+    now_time = now_time.strftime("%Y-%m-%d %H-%M-%S")
+    FORMAT = '%(asctime)s %(levelname)s: %(message)s'
+    logging.basicConfig(level=logging.INFO, filename = "/home/blacklutos/IGWS/wan_detect/log/detail/wan_state.log", filemode='a' ,format=FORMAT)
+    log_file = open("/home/blacklutos/IGWS/wan_detect/log/wan_state_" + now_time + ".log",'w')
 
-f = open('/home/blacklutos/IGWS/wan_detect/log/timestamp/timestamp.txt','w')
-f.write("wan_state_" + now_time + ".log")
-f.close()
-time.sleep(interval_time)
+    wan_state = ping('google.com',timeout=10)
+    if wan_state:
+        logging.info("Normal")
+        log_file.write("Normal")
+    else:
+        logging.info("Abnormal")
+        log_file.write("Abnormal")
+    log_file.close()
+
+    f = open('/home/blacklutos/IGWS/wan_detect/log/timestamp/timestamp.txt','w')
+    f.write("wan_state_" + now_time + ".log")
+    f.close()
+
+    f = open('/home/blacklutos/IGWS/wan_detect/interval.txt','r')
+    interval_time = int(f.read())
+    f.close()
+    time.sleep(interval_time)
