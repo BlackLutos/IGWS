5a7341f6376f4ea9ba33e228659682142dd6ae7f
BlackLutos <gcobs172250@gmail.com>
2022-11-03 10:43:00 +0800

    update

--- wan_detect/client.py
+++ wan_detect/client.py
@@ -4,6 +4,8 @@ import socket
 from monitor_wan_state import get_wan_state
 from get_bridge_info import get_bridge_ip
 from get_bridge_info import get_link_info
+import json
+from failover import failover
 
 def start_client(addr, port):
     PLC_ADDR = addr
@@ -13,17 +15,30 @@ def start_client(addr, port):
     count = 0
     pid = str(os.getpid())
     while True:
-        if get_wan_state() == True:
+        if get_wan_state() == "Normal":
             msg = 'Wan_State:Normal' + ", pid: " + pid
-        else:
+        elif get_wan_state() == "Abnormal":
             bridge_ip = get_bridge_ip()
             link_info = get_link_info()
             msg = 'Wan_State:Abnormal, ' + 'bridge IP: ' + bridge_ip + ', link_info: ' + link_info + ", pid: " + pid
+        else:
+            msg = 'Wan_State:Failover, pid: ' + pid
+
 
         msg = msg.encode(encoding='utf-8')
         s.send(msg)
         recv_data = s.recv(1024)
-        print(recv_data.decode(encoding='utf-8'))
+        if 'Normal' in str(recv_data):
+            print(recv_data.decode(encoding='utf-8'))
+        elif 'Failover' in str(recv_data):
+            print('abnormal')
+            print(recv_data.decode(encoding='utf-8'))
+            with open('config.json','r') as f:
+                config = json.load(f)
+            config['wan_state'] = 'Failover'
+            with open('config.json','w') as f:
+                f.write(json.dumps(config))
+            failover()
         time.sleep(3)
         # count += 1
         # if count > 2000:
@@ -32,4 +47,4 @@ def start_client(addr, port):
     s.close()
 
 if __name__ == '__main__':
-    start_client('127.0.0.1', 8801)
+    start_client('127.0.0.1', 8808)
--- wan_detect/server.py
+++ wan_detect/server.py
@@ -1,7 +1,7 @@
 import select
 import socket
 import json
-from failover import failover
+import os
 
 def start_server(port):
     HOST = '0.0.0.0'
@@ -9,58 +9,68 @@ def start_server(port):
 
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server.bind((HOST, PORT))   # 套接字绑定的IP与端口
-    server.listen(10)           # 开始TCP监听
+    server.bind((HOST, PORT))   
+    server.listen(10)           
 
-    inputs = [server]           # 存放需要被检测可读的socket
-    outputs = []                # 存放需要被检测可写的socket
+    inputs = [server]           
+    outputs = []                
 
     while inputs:
         readable, writable, exceptional = select.select(inputs, outputs, inputs)
         # 可读
         for s in readable:
-            if s is server:     # 可读的是server,说明有连接进入
+            if s is server:     
                 connection, client_address = s.accept()
                 inputs.append(connection)
             else:
-                data = s.recv(1024)        # 故意设置的这么小
+                data = s.recv(1024)        
                 if data:
-                    # 已经从这个socket当中收到数据, 如果你想写, 那么就将其加入到outputs中, 等到select模型检查它是否可写
+                   
                     print(data.decode(encoding='utf-8'))
                     info = data.decode(encoding='utf-8')
+                    failover_flag = False
                     print('-------------------------------')
-                    with open('config.json','r') as f:
-                        config = json.load(f)
-                    if (('Wan_State:Abnormal' in info) & (config['wan_state'] == 'Abnormal')):
-                        info = info.split(',',4)
-                        wan_state = (info[0].split(':',1))[1]
-                        bridge_ip = (info[1].split(' ',3))[3]
-                        with open('config.json','r') as f:
-                            config = json.load(f)
-                        config['wan_state'] = 'Failover' 
-                        with open('config.json','w') as f:
-                            f.write(json.dumps(config))
-                        failover()
+                    # with open('config.json','r') as f:
+                    #     config = json.load(f)
+                    # if (('Wan_State:Abnormal' in info) & (config['wan_state'] == 'Abnormal')):
+                    #     info = info.split(',',4)
+                    #     wan_state = (info[0].split(':',1))[1]
+                    #     bridge_ip = (info[1].split(' ',3))[3]
+                    #     with open('config.json','r') as f:
+                    #         config = json.load(f)
+                    #     config['wan_state'] = 'Failover' 
+                    #     with open('config.json','w') as f:
+                    #         f.write(json.dumps(config))
+                    #     failover()
+                    if ('Wan_State:Abnormal' in info):
+                        failover_flag = True
+                    elif ('Wan_State:Failover' in info):
+                        os.system('bash add_flow.sh')
+                        # print('Failover')
+
+
                     if s not in outputs:
                         outputs.append(s)
                 else:
-                    # 收到为空的数据,意味着对方已经断开连接,需要做清理工作
                     if s in outputs:
                         outputs.remove(s)
                     inputs.remove(s)
                     s.close()
 
-        # 可写
+        
         for w in writable:
-            w.send('Server received data'.encode(encoding='utf-8'))
+            if failover_flag:
+                w.send('Server received data: To do Failover'.encode(encoding='utf-8'))
+            else:
+                w.send('Server received data: Normal'.encode(encoding='utf-8'))
             outputs.remove(w)
 
-        # 异常
-        for s in exceptional:
-            inputs.remove(s)
-            if s in outputs:
-                outputs.remove(s)
-            s.close()
+        
+        # for s in exceptional:
+        #     inputs.remove(s)
+        #     if s in outputs:
+        #         outputs.remove(s)
+        #     s.close()
 
 if __name__ == '__main__':
-    start_server(8801)
+    start_server(8808)
